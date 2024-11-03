b8a451faa51dd5bc42bcd2b95d39a0ba7f66a24c
BOO-0-K <boo970328@gmail.com>
2024-11-03 14:06:30 +0900

    [CNG] React Helmet Title

--- src/routes/Repos.tsx
+++ src/routes/Repos.tsx
@@ -105,7 +105,7 @@ function Repos({ toggleDark, isDark }: IReposProps) {
     return (
         <Container>
             <Helmet>
-                <title>Repo Tracker | Repositories</title>
+                <title>Repo Tracker</title>
             </Helmet>
             <Header>
                 <Title>
